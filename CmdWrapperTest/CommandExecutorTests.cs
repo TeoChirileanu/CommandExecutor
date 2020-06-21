601fe5e92d25115d9aea2acdd985cb958d157c51
Theodore <teodorchirileanu@gmail.com>
2020-06-21 20:47:17 +0200

    add more success cases

--- CmdWrapperTest/CommandExecutorTests.cs
+++ CmdWrapperTest/CommandExecutorTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using CmdWrapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,7 +11,7 @@ namespace CmdWrapperTest
     public class CommandExecutorTests
     {
         [TestMethod]
-        public async Task ShouldExecuteCommand()
+        public async Task ShouldEcho()
         {
             // Arrange
             const string command = "echo hello";
@@ -20,6 +22,43 @@ namespace CmdWrapperTest
 
             // Assert
             Check.That(result).IsEqualTo("hello");
+        }
+
+        [TestMethod]
+        public async Task ShouldCreateFile()
+        {
+            // Arrange
+            const string file = "foo";
+            const string command = "echo hello > " + file;
+            using ICommandExecutor executor = new CommandExecutor();
+
+            // Act
+            var result = await executor.ExecuteCommand(command);
+
+            // Assert
+            Check.That(result).IsEmpty();
+            Check.That(File.Exists(file)).IsTrue();
+            
+            // Clean
+            File.Delete(file);
+        }
+
+        [TestMethod]
+        public async Task ShouldExecuteCommandInsideFile()
+        {
+            // Arrange
+            const string file = "foo";
+            await File.WriteAllTextAsync(file, "hostname");
+            using ICommandExecutor executor = new CommandExecutor();
+
+            // Act
+            var result = await executor.ExecuteCommand(file);
+
+            // Assert
+            Check.That(result.Contains(Environment.MachineName, StringComparison.InvariantCultureIgnoreCase)).IsTrue();
+            
+            // Clean
+            File.Delete(file);
         }
     }
 }
